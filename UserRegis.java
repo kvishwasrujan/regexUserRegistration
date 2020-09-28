3df81f268caed583317ab267c33c5461c544256e
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-28 19:15:03 +0530

    Added UserRegistration UC7

--- UserRegis.java
+++ UserRegis.java
@@ -4,16 +4,14 @@ package regexJava;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexUC6 {
-	// main method
+public class RegexUC7 {
 	public static void main(String[] args) {
 		// condition for Password
-		final String PASSWORD = "^(?=.*[A-Z])[a-zA-Z0-9]{8}$";
+		final String PASSWORD = "^(?=.*[A-Z])(?.*[0-9])[a-zA-Z0-9]{8}$";
 		final String INPUT = "Adeu35bg";
 		Pattern pattern = Pattern.compile(PASSWORD);
 		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
 	}
-
 }
 
