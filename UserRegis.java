b2343153563bd6d9157b510444d2ca7a9490d0a8
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-28 19:18:12 +0530

    Added UserRegistration UC8

--- UserRegis.java
+++ UserRegis.java
@@ -4,14 +4,13 @@ package regexJava;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexUC7 {
+public class RegexUC8 {
 	public static void main(String[] args) {
 		// condition for Password
-		final String PASSWORD = "^(?=.*[A-Z])(?.*[0-9])[a-zA-Z0-9]{8}$";
+		final String PASSWORD = "^(?=.*[A-Z])(?.*[0-9])(?.*[!@#$%^&*])[a-zA-Z0-9]{8}$";
 		final String INPUT = "Adeu35bg";
 		Pattern pattern = Pattern.compile(PASSWORD);
 		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
 	}
 }
-
