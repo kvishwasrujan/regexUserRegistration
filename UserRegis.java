9c5304e1c5f208ad22461903e0686ecf8ed60d41
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-28 18:52:29 +0530

    Added UserRegistration UC2

--- UserRegis.java
+++ UserRegis.java
@@ -4,15 +4,14 @@ package regexJava;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class regex {
-	// main method
+public class regexUC2 {
+//main method
 	public static void main(String[] args) {
-		// compare pattern
-		final String s = "^[A-Z]{1}[a-zA-Z]{2,}$";
-		final String i = "Aer";
-		Pattern pattern = Pattern.compile(s);
-		Matcher matcher = pattern.matcher(i);
-		// print message
+		// condition for LastName
+		final String LASTNAME = "^[A-Z]{1}[a-zA-Z]{2,}$";
+		final String INPUT = "Aedffr";
+		Pattern pattern = Pattern.compile(LASTNAME);
+		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
 	}
 
