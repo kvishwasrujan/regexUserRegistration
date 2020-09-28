6a2d990106198c9b28a22105181674a1e16d2356
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-28 19:01:57 +0530

    Added UserRegistration UC4

--- UserRegis.java
+++ UserRegis.java
@@ -4,15 +4,16 @@ package regexJava;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class regexUC3 {
+public class regexUC4 {
+	// main method
 	public static void main(String[] args) {
-		// main method
-		// condition for Email
-		final String Email = "^abc.{4}[a-z]{3}@bl.co.{7}[a-z]{2}$";
-		final String INPUT = "abc.xyz@bl.co.ww";
-		Pattern pattern = Pattern.compile(Email);
+		// condition for PhoneNumber
+		final String PHONE_NUM = "^[0-9]{2}[\\s-]{1}[0-9]{10}$";
+		final String INPUT = "91 1234567891";
+		Pattern pattern = Pattern.compile(PHONE_NUM);
 		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
 	}
 
 }
+
