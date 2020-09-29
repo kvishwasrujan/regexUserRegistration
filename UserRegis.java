1e4e9c6797d3b8509f09cdc7393d62d29eb006e1
kvishwasrujan <71625158+kvishwasrujan@users.noreply.github.com>
2020-09-29 11:19:46 +0530

    Update UserRegis.java

--- UserRegis.java
+++ UserRegis.java
@@ -1,6 +1,7 @@
 package regexJava;
 
 //importing packages
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,7 +10,9 @@ public class regexUC5 {
 	public static void main(String[] args) {
 		// condition for Password
 		final String PASSWORD = "^[A-Za-z0-9]{8}$";
-		final String INPUT = "Aedffr4e";
+	        Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the Password");
+		String INPUT=sc.nextLine();
 		Pattern pattern = Pattern.compile(PASSWORD);
 		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
