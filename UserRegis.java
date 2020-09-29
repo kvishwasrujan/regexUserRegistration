3f4edbcbe5f1253539cfc1f9fbe2b0027ab35540
kvishwasrujan <71625158+kvishwasrujan@users.noreply.github.com>
2020-09-29 11:21:30 +0530

    Update UserRegis.java

--- UserRegis.java
+++ UserRegis.java
@@ -1,14 +1,17 @@
 package regexJava;
 
 //importing packages
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUC8 {
 	public static void main(String[] args) {
 		// condition for Password
+		Scanner sc= new Scanner(System.in);
 		final String PASSWORD = "^(?=.*[A-Z])(?.*[0-9])(?.*[!@#$%^&*])[a-zA-Z0-9]{8}$";
-		final String INPUT = "Adeu35bg";
+		System.out.println("Enter the Password");
+		String INPUT=sc.nextLine();
 		Pattern pattern = Pattern.compile(PASSWORD);
 		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
