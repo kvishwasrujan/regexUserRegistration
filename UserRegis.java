b4de355a8c0bf5111b8db07454eaeb477395fdb3
kvishwasrujan <71625158+kvishwasrujan@users.noreply.github.com>
2020-09-29 11:15:28 +0530

    Update UserRegis.java

--- UserRegis.java
+++ UserRegis.java
@@ -1,15 +1,19 @@
 package regexJava;
 
 //importing packages
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class regexUC2 {
 //main method
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
 		// condition for LastName
 		final String LASTNAME = "^[A-Z]{1}[a-zA-Z]{2,}$";
-		final String INPUT = "Aedffr";
+		System.out.println("Enter the Password");
+		String INPUT=sc.nextLine();
+		
 		Pattern pattern = Pattern.compile(LASTNAME);
 		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
