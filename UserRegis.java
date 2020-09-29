4c6d5cb80470ef9f9c99232d505719e66fed34b2
kvishwasrujan <71625158+kvishwasrujan@users.noreply.github.com>
2020-09-29 11:18:01 +0530

    Update UserRegis.java

--- UserRegis.java
+++ UserRegis.java
@@ -1,15 +1,18 @@
 package regexJava;
 
 //importing packages
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class regexUC4 {
 	// main method
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
 		// condition for PhoneNumber
 		final String PHONE_NUM = "^[0-9]{2}[\\s]{1}[0-9]{10}$";
-		final String INPUT = "91 1234567891";
+		System.out.println("Enter the PhoneNum");
+		String INPUT=sc.nextLine();
 		Pattern pattern = Pattern.compile(PHONE_NUM);
 		Matcher matcher = pattern.matcher(INPUT);
 		System.out.println("Input String matches regex-" + matcher.matches());
